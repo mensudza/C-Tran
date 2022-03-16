8b9e74cbdf3a5f60caab0de9d739be7c60baa32d
mensudza <74386100+mensudza@users.noreply.github.com>
2022-03-16 16:02:04 +0700

    Update voc2007_20.py
    for custom data

--- dataloaders/voc2007_20.py
+++ dataloaders/voc2007_20.py
@@ -12,40 +12,52 @@ import torchvision.transforms.functional as TF
 import random
 import csv
 from dataloaders.data_utils import get_unk_mask_indices
+import pandas as pd
 
 
 
 from xml.dom.minidom import parse 
 import xml.dom.minidom
-category_info = {'aeroplane':0, 'bicycle':1, 'bird':2, 'boat':3, 'bottle':4,
-                 'bus':5, 'car':6, 'cat':7, 'chair':8, 'cow':9,
-                 'diningtable':10, 'dog':11, 'horse':12, 'motorbike':13, 'person':14,
-                 'pottedplant':15, 'sheep':16, 'sofa':17, 'train':18, 'tvmonitor':19}
+category_info = {'none': 0, 'camera':1, 'garage':2, 'car':3, 'shrine':4,
+                 'garden':5}
 
 class Voc07Dataset(torch.utils.data.Dataset):
-    def __init__(self, img_dir='./data/VOCdevkit/VOC2007/JPEGImages', anno_path='./data/VOCdevkit/VOC2007/Main/trainval.txt', image_transform=None, labels_path='./data/VOCdevkit/VOC2007/Annotations',known_labels=0,testing=False,use_difficult=False):
+    def __init__(self, df, img_dir='./data/VOCdevkit/VOC2007/JPEGImages', anno_path='./data/VOCdevkit/VOC2007/Main/trainval.txt', image_transform=None, labels_path='./data/VOCdevkit/VOC2007/Annotations',known_labels=0,testing=False,use_difficult=False):
         self.img_names  = []
-        with open(anno_path, 'r') as f:
-             self.img_names = f.readlines()
+#         with open(anno_path, 'r') as f:
+#              self.img_names = f.readlines()
+        for n in list(df['filename']):
+          self.img_names.append(n)
+        
         self.img_dir = img_dir
 
-        self.num_labels = 20
+        self.num_labels = 6
         self.known_labels = known_labels
         self.testing=testing
         
         self.labels = []
-        for name in self.img_names:
-            label_file = os.path.join(labels_path,name[:-1]+'.xml')
-            label_vector = np.zeros(self.num_labels)
-            DOMTree = xml.dom.minidom.parse(label_file)
-            root = DOMTree.documentElement
-            objects = root.getElementsByTagName('object')  
-            for obj in objects:
-                if (not use_difficult) and (obj.getElementsByTagName('difficult')[0].firstChild.data) == '1':
-                    continue
-                tag = obj.getElementsByTagName('name')[0].firstChild.data.lower()
-                label_vector[int(category_info[tag])] = 1.0
-            self.labels.append(label_vector)
+        
+        for l in list(df['labels']):
+           label_vector = np.zeros(self.num_labels) #initial vector
+           ll = l.split(' ')
+           for e in ll:
+              label_vector[int(e)] = 1.0
+           self.labels.append(label_vector)
+       
+        
+        
+#         for name in self.img_names:
+#             label_file = os.path.join(labels_path,name[:-1]+'.xml')
+#             label_vector = np.zeros(self.num_labels)
+#             DOMTree = xml.dom.minidom.parse(label_file)
+#             root = DOMTree.documentElement
+#             objects = root.getElementsByTagName('object')  
+#             for obj in objects:
+#                 if (not use_difficult) and (obj.getElementsByTagName('difficult')[0].firstChild.data) == '1':
+#                     continue
+#                 tag = obj.getElementsByTagName('name')[0].firstChild.data.lower()
+#                 label_vector[int(category_info[tag])] = 1.0
+#             self.labels.append(label_vector)
         
         # self.labels = np.array(self.labels).astype(np.float32)
         self.labels = np.array(self.labels).astype(int)
@@ -54,7 +66,7 @@ class Voc07Dataset(torch.utils.data.Dataset):
         
 
     def __getitem__(self, index):
-        name = self.img_names[index][:-1]+'.jpg'
+        name = self.img_names[index]+'.jpg'
         image = Image.open(os.path.join(self.img_dir, name)).convert('RGB')
           
         if self.image_transform:
